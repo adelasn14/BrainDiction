34a5da4b55bd696b3d530710580771ebe42c52b5
charestavd <80313988+charestavd@users.noreply.github.com>
2022-06-01 00:52:57 +0700

    fixed radio button and some trouble in pickdate
    masih error di bagian pick tanggalnya karena init

--- app/src/main/java/com/example/braindiction/ui/patient/NewPredictionActivity.kt
+++ app/src/main/java/com/example/braindiction/ui/patient/NewPredictionActivity.kt
@@ -1,11 +1,16 @@
 package com.example.braindiction.ui.patient
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
+import com.example.braindiction.R
 import com.example.braindiction.databinding.ActivityNewPredictionBinding
 import com.example.braindiction.ui.main.home.HomeActivity
+import java.util.*
 
 class NewPredictionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewPredictionBinding
@@ -19,6 +24,7 @@ class NewPredictionActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         setupAction()
+        setChangeGender()
     }
 
     private fun setMyButtonEnable() {
@@ -59,4 +65,33 @@ class NewPredictionActivity : AppCompatActivity() {
         startActivity(backTo)
         return true
     }
+
+    //button set gender change
+    private fun setChangeGender() {
+        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            val text = "You selected: " +
+                    if (R.id.radioButton == checkedId)
+                        "male"
+                    else
+                        "female"
+            Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // datepicker
+//    private fun setupDatePicker() {
+//        val today = Calendar.getInstance()
+//        binding.PickDate.(
+//            today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH)
+//
+//        ) { view, year, monthOfYear, dayOfMonth ->
+//            val month = monthOfYear + 1
+//            val msg = "Selected Date is $dayOfMonth/$month/$year"
+//            Toast.makeText(this@New, msg, Toast.LENGTH_SHORT).show()
+//
+//            binding.textDateSend.text = msg
+//            binding.textDateSend.visibility = View.VISIBLE
+//        }
+//    }
+
 }
